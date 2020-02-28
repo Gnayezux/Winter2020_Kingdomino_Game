aac87b70ce0eebfde70e70b8f325965495286481
Abdallah Shapsough <abshapsough@gmail.com>
2020-02-27 22:48:31 -0500

    completed feature 4 without javadoc

--- ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/controller/KingdominoController.java
+++ ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/controller/KingdominoController.java
@@ -2,14 +2,119 @@ package ca.mcgill.ecse223.kingdomino.controller;
 
 import ca.mcgill.ecse223.kingdomino.KingdominoApplication;
 import ca.mcgill.ecse223.kingdomino.model.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class KingdominoController {
 	
 	public KingdominoController() {
 		
 	}
+	public static void main(String args[]) {
+		// Intialize empty game
+		Kingdomino kingdomino = new Kingdomino();
+		Game game = new Game(48, kingdomino);
+		game.setNumberOfPlayers(4);
+		kingdomino.setCurrentGame(game);
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("src/main/resources/alldominoes.dat"));
+			String line = "";
+			String delimiters = "[:\\+()]";
+			while ((line = br.readLine()) != null) {
+				String[] dominoString = line.split(delimiters); // {id, leftTerrain, rightTerrain, crowns}
+				int dominoId = Integer.decode(dominoString[0]);
+				TerrainType leftTerrain = getTerrainType(dominoString[1]);
+				TerrainType rightTerrain = getTerrainType(dominoString[2]);
+				int numCrown = 0;
+				if (dominoString.length > 3) {
+					numCrown = Integer.decode(dominoString[3]);
+				}
+				new Domino(dominoId, leftTerrain, rightTerrain, numCrown, game);
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new java.lang.IllegalArgumentException(
+					"Error occured while trying to read alldominoes.dat: " + e.getMessage());
+		}
+		
+//		System.out.print(game.getAllDominos());
+//		System.out.print(BrowseDominoPile(kingdomino));
+//		System.out.print(BrowseDomino(1,kingdomino));
+		System.out.print(BrowseFilteredDominos("mountain", kingdomino));
+	}
 	
+	private static TerrainType getTerrainType(String terrain) {
+		switch (terrain) {
+		case "W":
+			return TerrainType.WheatField;
+		case "F":
+			return TerrainType.Forest;
+		case "M":
+			return TerrainType.Mountain;
+		case "G":
+			return TerrainType.Grass;
+		case "S":
+			return TerrainType.Swamp;
+		case "L":
+			return TerrainType.Lake;
+		default:
+			throw new java.lang.IllegalArgumentException("Invalid terrain type: " + terrain);
+		}
+	}
+	
+	
+	private static TerrainType getTerrainTypeFilter(String terrain) {
+		terrain = terrain.toLowerCase();
+		if(terrain.equals("wheatfield")) {
+			terrain = "wheat";
+		}
+		switch (terrain) {
+		case "wheat":
+			return TerrainType.WheatField;
+		case "forest":
+			return TerrainType.Forest;
+		case "mountain":
+			return TerrainType.Mountain;
+		case "grass":
+			return TerrainType.Grass;
+		case "swamp":
+			return TerrainType.Swamp;
+		case "lake":
+			return TerrainType.Lake;
+		default:
+			throw new java.lang.IllegalArgumentException("Invalid terrain type: " + terrain);
+		}
+	}
+	
+	//completed browse filtered domino with help of getTerrainTypeFilter method
+	public static List<Domino> BrowseFilteredDominos(String terrain,Kingdomino kingdomino) {
+		ArrayList<Domino> allDominos = new ArrayList<Domino>(kingdomino.getCurrentGame().getAllDominos());
+		Collections.sort(allDominos, (a, b) -> a.getId()-b.getId());
+		List<Domino> filteredList = (allDominos.stream().filter(domino -> domino.getLeftTile().equals(getTerrainTypeFilter(terrain)) || domino.getRightTile().equals(getTerrainTypeFilter(terrain))).collect(Collectors.toList()));
+		return filteredList;
+
+	}
+	
+	//completed browse single domino
+	public static Domino BrowseDomino(int id,Kingdomino kingdomino) {
+		ArrayList<Domino> allDominos = new ArrayList<Domino>(kingdomino.getCurrentGame().getAllDominos());
+		Collections.sort(allDominos, (a, b) -> a.getId()-b.getId());
+		return allDominos.get(id-1);
+	}
+	
+	
+	//completed ordered browse domino pile
+	public static ArrayList<Domino> BrowseDominoPile(Kingdomino kingdomino) {
+		ArrayList<Domino> allDominos = new ArrayList<Domino>(kingdomino.getCurrentGame().getAllDominos());
+		Collections.sort(allDominos, (a, b) -> a.getId()-b.getId());
+		return allDominos;
+	}
 	// Calculating the ranking of the players in the game
 	public static void calculateRanking(Kingdomino kingdomino) {
 		//Kingdomino kingdomino = KingdominoApplication.getKingdomino();
