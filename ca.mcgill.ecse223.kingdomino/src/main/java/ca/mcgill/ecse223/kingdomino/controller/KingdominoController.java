a344cd4348e8e2de320c7bf0f194b4930c5a0123
VictoriaIannotti <iannotti.victoria@gmail.com>
2020-03-04 18:21:59 -0500

    code for features 23 and 24

--- ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/controller/KingdominoController.java
+++ ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/controller/KingdominoController.java
@@ -12,19 +12,57 @@ public class KingdominoController {
 	
 	// Calculating the ranking of the players in the game
 	public static void calculateRanking(Kingdomino kingdomino) {
-		//Kingdomino kingdomino = KingdominoApplication.getKingdomino();
 		List <Player> players = kingdomino.getCurrentGame().getPlayers();
-		//nfhadsjf asfsadoijg sdai;
-		for (int i = 1; i < players.size(); i++) {
-			Player currentPlayer = players.get(i);
-			int j = i - 1;
-			while (j>=0 && currentPlayer.getTotalScore() < players.get(j).getTotalScore()) {
-				players.set(j+1, players.get(j));
-				j--;
+		Player currentPlayer;
+		Player tempPlayer;
+		for(int i = 0; i<players.size()-1; i++) {
+			currentPlayer = players.get(i);
+			for (int j = i+1; j<players.size(); j++) {
+				tempPlayer = players.get(j);
+				if(tempPlayer.getTotalScore()>currentPlayer.getTotalScore()) {
+					players.set(i, tempPlayer);
+					players.set(j, currentPlayer);
+					currentPlayer = tempPlayer;
+				} else if (tempPlayer.getTotalScore()==currentPlayer.getTotalScore()) {
+					if(getLargestPropertySize(tempPlayer.getKingdom().getProperties()) > getLargestPropertySize(currentPlayer.getKingdom().getProperties())) {
+						players.set(i, tempPlayer);
+						players.set(j, currentPlayer);
+						currentPlayer = tempPlayer;
+					} else if (getNumberCrowns(tempPlayer.getKingdom().getProperties()) > getNumberCrowns(currentPlayer.getKingdom().getProperties())) {
+						players.set(i, tempPlayer);
+						players.set(j, currentPlayer);
+						currentPlayer = tempPlayer;
+					}
+				}
 			}
-			players.set(j+1, currentPlayer);
-			players.get(i-1).setCurrentRanking(i);
 		}
+		for (int i = 0; i < players.size(); i++) {
+			players.get(i).setCurrentRanking(i+1);
+		}
+	}
+	
+	private static int getLargestPropertySize(List<Property> properties) {
+		if(properties == null) {
+			return 0;
+		}
+		Property largestProperty = properties.get(0);
+		if(properties.size()==1) {
+			return largestProperty.getSize();
+		}
+		for(int i = 1; i<properties.size(); i++) {
+			if(properties.get(i).getSize()>largestProperty.getSize()) {
+				largestProperty = properties.get(i);
+			}
+		}
+		return largestProperty.getSize();
+		
+	}
+	private static int getNumberCrowns(List<Property> properties) {
+		int numCrowns=0;
+		for(Property property : properties) {
+			numCrowns+=property.getCrowns();
+		}
+		return numCrowns;
 	}
 	
 }
