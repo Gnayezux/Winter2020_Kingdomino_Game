e2cc751860e16c0091c69f4fd31e3548ae8b0e01
Kaicheng Wu <kaichengwu8101@hotmail.com>
2020-03-04 20:39:06 -0500

    finished features 20-22, having some difficulties with 19

--- ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/controller/KingdominoController.java
+++ ca.mcgill.ecse223.kingdomino/src/main/java/ca/mcgill/ecse223/kingdomino/controller/KingdominoController.java
@@ -2,6 +2,8 @@ package ca.mcgill.ecse223.kingdomino.controller;
 
 import ca.mcgill.ecse223.kingdomino.KingdominoApplication;
 import ca.mcgill.ecse223.kingdomino.model.*;
+import ca.mcgill.ecse223.kingdomino.model.DominoInKingdom.DirectionKind;
+
 import java.util.*;
 
 public class KingdominoController {
@@ -26,17 +28,55 @@ public class KingdominoController {
 			players.get(i-1).setCurrentRanking(i);
 		}
 	}
-	
 
+
+	public DominoInKingdom rightTile (DominoInKingdom leftTile) {
+		DominoInKingdom rightTile = new DominoInKingdom(leftTile.getX(), leftTile.getY(), leftTile.getKingdom(), leftTile.getDomino());
+		TerrainType terr = leftTile.getDomino().getRightTile();
+		if(leftTile.getDirection()==DirectionKind.Up) {
+			rightTile.setY(leftTile.getY()+1);
+		}
+		else if (leftTile.getDirection() == DirectionKind.Down) {
+			rightTile.setY(leftTile.getY()-1);
+		}
+		else if (leftTile.getDirection() == DirectionKind.Left) {
+			rightTile.setX(leftTile.getX()-1);
+		}
+		else {
+			rightTile.setX(leftTile.getX()+1);
+		}
+		
+		return rightTile;
+	}
+	
+	public boolean isConnected(DominoInKingdom d1, DominoInKingdom d2) {
+		if (d1.getX()==d2.getX()&&d1.getY()==d2.getY()-1) {
+			return true;
+		}
+		else if (d1.getX()==d2.getX()&&d1.getY()==d2.getY()+1) {
+			return true;
+		}
+		else if (d1.getY()==d2.getY()&&d1.getX()==d2.getX()-1) {
+			return true;
+		}
+		else if (d1.getY()==d2.getY()&&d1.getX()==d2.getX()+1) {
+			return true;
+		}
+		
+		return false;	
+	}
+	
 	public Property[] IdentifyKingdomProperties(DominoInKingdom[] playedDominoes, Kingdom aKingdom){
 		
 		
 		Property[] myProperties= new Property[playedDominoes.length];
 		for(int i=0; i<playedDominoes.length; i++) {
 			Property aProperty = new Property(aKingdom);
-			for(int j=i+1; j<playedDominoes.length; j++) {
-				if(playedDominoes[i].getX()==playedDominoes[j].getX()+1) {
-					
+			for(int j=0; j<playedDominoes.length; j++) {
+				if(isConnected(playedDominoes[i],playedDominoes[j])) {
+					if(playedDominoes[i].getDomino().getLeftTile()==playedDominoes[j].getDomino().getLeftTile()) {
+						
+					}
 				}
 			
 			aProperty.setLeftTile(playedDominoes[i].getDomino().getLeftTile());
@@ -66,10 +106,33 @@ public class KingdominoController {
 		aProperty.setSize(size);
 	}
 	
-	public int CalculateBonusScore(DominoInKingdom[] playedDominoes, Kingdom aKingdom) {
+	public int CalculateBonusScore(DominoInKingdom[] playedDominoes, Kingdom aKingdom, Castle castle) {
 		int bonus = 0;
+		boolean middlexl = false;
+		boolean middlexr = false;
+		boolean middleyt = false;
+		boolean middleyb = false;
 		if (playedDominoes.length ==12) {
 			bonus +=5;
+		}
+		
+		for (int i=0; i<playedDominoes.length; i++) {
+			if(playedDominoes[i].getX()==castle.getX()-2) {
+				middlexl = true;
+				}
+			if (playedDominoes[i].getX()==castle.getX()+2) {
+				middlexr = true;
+			}
+			if (playedDominoes[i].getY()==castle.getY()-2) {
+				middleyb = true;
+			}
+			if (playedDominoes[i].getY()==castle.getY()+2) {
+				middleyt = true;
+			}
+			
+		}
+		if(middlexl&&middlexr&&middleyb&&middleyt) {
+			bonus+=10;
 		}
 		
 			
