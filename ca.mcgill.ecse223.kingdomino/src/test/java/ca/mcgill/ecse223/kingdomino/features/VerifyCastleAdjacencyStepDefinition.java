6a35e6233da267dc1813541bb7aa8de2c9a45970
Abdallah Shapsough <abshapsough@gmail.com>
2020-03-07 18:17:00 -0500

    verify

--- ca.mcgill.ecse223.kingdomino/src/test/java/ca/mcgill/ecse223/kingdomino/features/VerifyCastleAdjacencyStepDefinition.java
+++ ca.mcgill.ecse223.kingdomino/src/test/java/ca/mcgill/ecse223/kingdomino/features/VerifyCastleAdjacencyStepDefinition.java
@@ -14,22 +14,24 @@ import ca.mcgill.ecse223.kingdomino.controller.KingdominoController;
 import static org.junit.Assert.fail;
 import static org.junit.Assert.assertEquals;
 
+
 public class VerifyCastleAdjacencyStepDefinition {
 	int x;
 	int y;
 	DirectionKind direction;
 	boolean result;
+	
 	@Given("the game is initialized for castle adjacency")
 	public void the_game_is_initialized_for_castle_adjacency() {
 	    Kingdomino aKingdomino = new Kingdomino();
 	    //throw new cucumber.api.PendingException();
 	}
 
-	@Given("the current player preplaced the domino with ID {int} at position {int}:{int} and direction {direction}")
-	public void the_current_player_preplaced_the_domino_with_ID_at_position_and_direction(Integer int1, Integer int2, Integer int3, DirectionKind aDirection) {
+	@Given("the current player preplaced the domino with ID {int} at position {int}:{int} and string {String}")
+	public void the_current_player_preplaced_the_domino_with_ID_at_position_and_direction(Integer int1, Integer int2, Integer int3, String string) {
 	    this.x = int2;
 	    this.y = int3;
-	    this.direction = aDirection;
+	    this.direction = getDirection(string);
 	    
 	}
 
@@ -43,9 +45,36 @@ public class VerifyCastleAdjacencyStepDefinition {
 	public void the_castle_domino_adjacency_is(String string) {
 	    // Write code here that turns the phrase above into concrete actions
 	    if (this.result) {
-	    	assertEquals(this.result,true);
+	    	assertEquals(this.result,getValidity(string));
 	    } else {
 	    	fail();
 	    }
+	}
+	
+	private DirectionKind getDirection(String dir) {
+		switch (dir) {
+		case "up":
+			return DirectionKind.Up;
+		case "down":
+			return DirectionKind.Down;
+		case "left":
+			return DirectionKind.Left;
+		case "right":
+			return DirectionKind.Right;
+		default:
+			throw new java.lang.IllegalArgumentException("Invalid direction: " + dir);
+		}
+	}
+	
+	private Boolean getValidity(String string) {
+		switch (string) {
+		case "valid":
+			return true;
+		case "invalid":
+			return false;
+		default:
+			throw new java.lang.IllegalArgumentException("Invalid validity: " + string);
+		}
+		
 	}
 }
