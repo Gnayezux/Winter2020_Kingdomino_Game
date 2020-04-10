5ccddaa4316674564fb1d48f88e02fe876384b4f
Gnayezux <59747067+Gnayezux@users.noreply.github.com>
2020-04-10 17:19:54 -0400

    PlacingDomino, PlacingLast|Domino Step Definitions (Still working on it)
    A completed but not yet debugged version, as several errors pop up during feature test. Require furthur improvement

---
+++ ca.mcgill.ecse223.kingdomino/src/test/java/ca/mcgill/ecse223/kingdomino/features/PlacingLastDominoStepDefinition.java
@@ -0,0 +1,130 @@
+package ca.mcgill.ecse223.kingdomino.features;
+
+import static org.junit.Assert.assertEquals;
+
+import ca.mcgill.ecse223.kingdomino.KingdominoApplication;
+import ca.mcgill.ecse223.kingdomino.controller.KingdominoController;
+import ca.mcgill.ecse223.kingdomino.model.Castle;
+import ca.mcgill.ecse223.kingdomino.model.Domino;
+import ca.mcgill.ecse223.kingdomino.model.DominoInKingdom;
+import ca.mcgill.ecse223.kingdomino.model.Game;
+import ca.mcgill.ecse223.kingdomino.model.Kingdom;
+import ca.mcgill.ecse223.kingdomino.model.Kingdomino;
+import ca.mcgill.ecse223.kingdomino.model.Player;
+import ca.mcgill.ecse223.kingdomino.model.User;
+import ca.mcgill.ecse223.kingdomino.model.Domino.DominoStatus;
+import ca.mcgill.ecse223.kingdomino.model.DominoInKingdom.DirectionKind;
+import ca.mcgill.ecse223.kingdomino.model.Player.PlayerColor;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PlacingLastDominoStepDefinition {
+	Domino dom;
+	/**
+	 * @author Zeyang Xu
+	 */
+	@Given("the game has been initialized for placing last domino")
+	public void the_game_has_been_initialized_for_placing_last_domino() {
+		Kingdomino kingdomino = new Kingdomino();
+		Game game = new Game(48, kingdomino);
+		game.setNumberOfPlayers(4);
+		kingdomino.setCurrentGame(game);
+		// Populate game
+		addDefaultUsersAndPlayers(game);
+		KingdominoController.createAllDominos(game);
+		game.setNextPlayer(game.getPlayer(0));
+		KingdominoApplication.setKingdomino(kingdomino);
+	}
+	
+	/**
+	 * @author Zeyang Xu
+	 */
+	
+	@Given("it is the last turn of the game")
+	public void it_is_the_last_turn_of_the_game() {
+		assertEquals(KingdominoApplication.getKingdomino().getAllGame(0).hasNextDraft(),false);
+	}
+	
+	/**
+	 * @author Zeyang Xu
+	 */
+	
+	@Given("the current player is not the last player in the turn")
+	public void the_current_player_is_not_the_last_player_in_the_turn() {
+		assertEquals(KingdominoApplication.getKingdomino().getAllGame(0).hasNextPlayer(),true);
+	}
+	
+	/**
+	 * @author Zeyang Xu
+	 */
+	
+	@Given("the current player is preplacing his/her domino with ID 6 at location 2:2 with direction \"down\"")
+	public void the_current_player_is_preplacing_their_domino_with_ID6_at_location_22_with_direction_down() {
+		Player player = KingdominoApplication.getKingdomino().getCurrentGame().getNextPlayer();
+		dom = KingdominoApplication.getKingdomino().getCurrentGame().getAllDomino(6 - 1);
+		DominoInKingdom domIn = new DominoInKingdom(2, 2, player.getKingdom(), dom);
+		domIn.setDirection(getDirection("down"));
+		player.getKingdom().addTerritory(domIn);
+	}
+	
+	/**
+	 * @author Zeyang Xu
+	 */
+	
+	@And("the preplaced domino has the status \"CorrectlyPreplaced\"")
+	public void the_preplaced_domino_has_the_status_CorrectlyPreplaced() {
+		assertEquals(dom.getStatus(),DominoStatus.CorrectlyPreplaced);
+	}
+	
+	/**
+	 * @author Zeyang Xu
+	 */
+	
+	@When("the current player places his/her domino")
+	public void the_current_player_places_their_domino() {
+		assertEquals(KingdominoController.placeDomino(KingdominoApplication.getKingdomino()),true);
+	}
+	@Then("this player now shall be placing his/her domino ")
+	public void this_player_now_shall_be_placing_their_domino() {
+		assertEquals(KingdominoController.ChooseNextDomino(KingdominoApplication.getKingdomino().getCurrentGame().getNextPlayer(), KingdominoApplication.getKingdomino(), 6),true);
+	}
+	
+	@Given("the current player is the last player in the turn")
+	public void the_current_player_is_the_last_player_in_the_turn() {
+		assertEquals(KingdominoApplication.getKingdomino().getCurrentGame().getNextPlayer(), null);
+	}
+	
+	
+	/**********************
+	 * * Helper Methods * *
+	 **********************/
+	
+	private void addDefaultUsersAndPlayers(Game game) {
+		String[] userNames = { "User1", "User2", "User3", "User4" };
+		for (int i = 0; i < userNames.length; i++) {
+			User user = game.getKingdomino().addUser(userNames[i]);
+			Player player = new Player(game);
+			player.setUser(user);
+			player.setColor(PlayerColor.values()[i]);
+			Kingdom kingdom = new Kingdom(player);
+			new Castle(0, 0, kingdom, player);
+		}
+	}
+	private DirectionKind getDirection(String dir) {
+		switch (dir) {
+		case "up":
+			return DirectionKind.Up;
+		case "down":
+			return DirectionKind.Down;
+		case "left":
+			return DirectionKind.Left;
+		case "right":
+			return DirectionKind.Right;
+		default:
+			throw new java.lang.IllegalArgumentException("Invalid direction: " + dir);
+		}
+	}
+
+}
